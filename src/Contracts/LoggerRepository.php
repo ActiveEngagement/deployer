9b28da0d4f751e619a0442eaf44a97adf145b73b
Jacob Lockard <jacoblockard99@gmail.com>
2022-08-11 14:02:38 -0400

    Document LoggerRepository

--- src/Contracts/LoggerRepository.php
+++ src/Contracts/LoggerRepository.php
@@ -4,8 +4,25 @@ namespace Actengage\Deployer\Contracts;
 
 use Psr\Log\LoggerInterface;
 
+/**
+ * Repository for a logger.
+ * 
+ * Defines a class that is capable of storing and accessing a `LoggerInterface`.
+ */
 interface LoggerRepository
 {
+    /**
+     * Should get the logger.
+     * 
+     * @return LoggerInterface
+     */
     function get(): LoggerInterface;
+
+    /**
+     * Should set the logger.
+     * 
+     * @param LoggerInterface $logger
+     * @return void
+     */
     function set(LoggerInterface $logger): void;
 }
--- src/LoggerRepository.php
+++ src/LoggerRepository.php
@@ -6,6 +6,11 @@ use Actengage\Deployer\Contracts\LoggerRepository as LoggerRepositoryInterface;
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
 
+/**
+ * The package's logger repository.
+ * 
+ * A basic implementation of {@see Actengage\Deployer\Contracts\LoggerRepository}.
+ */
 class LoggerRepository implements LoggerRepositoryInterface
 {
     protected ?LoggerInterface $logger = null;
