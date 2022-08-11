2b438f174100f210e6cb42fb155c88033b18efa5
Jacob Lockard <jacoblockard99@gmail.com>
2022-08-11 15:33:40 -0400

    Write documentation
    Write some doc blocks for the new classes.

--- src/BundlePruner.php
+++ src/BundlePruner.php
@@ -3,8 +3,14 @@
 namespace Actengage\Deployer;
 
 use Actengage\Deployer\Contracts\BundlesRepository;
+use Actengage\Deployer\Contracts\LoggerRepository;
 use Actengage\Deployer\Contracts\PathProvider;
 
+/**
+ * Prunes old bundles.
+ * 
+ * A class that is capable of removing old bundles from the bundles directory.
+ */
 class BundlePruner
 {
     public function __construct
@@ -16,13 +22,24 @@ class BundlePruner
     {
     }
 
+    /**
+     * Prunes old bundles.
+     * 
+     * Removes all bundles from disk except for the given number of most recent ones to keep.
+     * 
+     * Note that bundles with missing or malformed manifests will not be included (because it is impossible to
+     * accurately determine their timestamps).
+     * 
+     * @param int $keep the number of most recent bundles to keep.
+     * @return int the number of bundles that were deleted.
+     */
     public function prune(int $keep): int
     {
         $deleted = 0;
 
         $bundlesToDelete = $this->bundles->all()->skip($keep);
         $bundlesToDelete->each(function (Bundle $bundle) use (&$deleted) {
-            $this->logger->get()->debug("Deleting bundle $bundle->path");
+            $this->logger->get()->debug("Deleting bundle at $bundle->path");
             $this->filesystem->delete($bundle->path);
             $deleted++;
         });
