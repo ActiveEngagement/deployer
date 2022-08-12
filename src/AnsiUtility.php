590e7016b39e78bdfd62ce3d6837a68792c47307
Jacob Lockard <jacoblockard99@gmail.com>
2022-08-12 13:07:37 -0400

    Polish AnsiUtility
    Polish AnsiUtility by:
      1. Removing the redundant AnsiCodeUtility class.
      2. Adding a bit of documentation.

--- src/AnsiUtility.php
+++ src/AnsiUtility.php
@@ -2,14 +2,32 @@
 
 namespace Actengage\Deployer;
 
+/**
+ * ANSI codes utility.
+ * 
+ * A class that is capable of working with, generating, and applying ANSI escape codes.
+ */
 class AnsiUtility
 {
-    public function __construct(protected AnsiCodeUtility $codes)
-    {
-    }
+    public const RESET = '0';
+    public const BOLD_ON = '1';
+    public const BOLD_OFF = '22';
 
     public function bold(string $input): string
     {
-        return $this->codes->make('1').$input.$this->codes->make('22');
+        return $this->code(self::BOLD_ON).$input.$this->code(self::BOLD_OFF);
+    }
+
+    /**
+     * Gets an ANSI code.
+     * 
+     * Gets an ANSI code with the given attribute "parameters."
+     * 
+     * @param string[] $params the attributes for which to generate a code.
+     * @return string the generated code.
+     */
+    public function code(string ...$params): string
+    {
+        return "\033[".implode(';', $params).'m';
     }
 }
